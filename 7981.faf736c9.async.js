ea4be28488980d605de5979a78aee6eddd19855f
Barrior <barrior@qq.com>
2024-01-19 09:38:13 +0000

    Deploying to gh-pages from @ Barrior/schema-render@c1644aa7f280a975f0e2e49c3652ceac5caa42a5 🚀

--- 7981.faf736c9.async.js
+++ 7981.faf736c9.async.js
@@ -1,6 +1,6 @@
 "use strict";(self.webpackChunkroot=self.webpackChunkroot||[]).push([[7981],{57981:function(a,n,e){e.r(n),e.d(n,{builtinRenderers:function(){return m}});var _=e(92935),t=e.n(_),r=e(29533),s,m=(0,r.H)(s||(s=t()([`
-  min-width: 550px;
-  font-size: 15px;
+  min-width: 580px;
+  font-size: 15px !important;
 
   .schema-render-form-actions {
     margin-left: 0 !important;
