1f93f1b01adeeafe28d165a555780c87ee517042
Barrior <barrior@qq.com>
2024-10-23 16:14:17 +0800

    feat(SearchTable): add locale files

--- packages/search-table-react/src/locale/en_US.ts
+++ packages/search-table-react/src/locale/en_US.ts
@@ -1,3 +1,16 @@
+import en_US from '@schema-render/search-react/dist/esm/locale/en_US'
+
 export default {
-  more: 'more',
+  ...en_US,
+  SearchTable: {
+    more: 'more',
+    settingTips: 'Columns Setting',
+    settingModalTitle: 'Columns Setting',
+    settingModalOk: 'Ok',
+    settingModalReset: 'Reset Current Setting',
+    settingModalResetDefault: 'Restore Defaults',
+    settingModalColumnEnum: ['Name', 'Display', 'Width', 'Fixed', 'Sort'],
+    longText: 'All',
+    longTextModalTitle: 'All',
+  },
 }
--- packages/search-table-react/src/locale/zh_CN.ts
+++ packages/search-table-react/src/locale/zh_CN.ts
@@ -1,3 +1,16 @@
+import zh_CN from '@schema-render/search-react/dist/esm/locale/zh_CN'
+
 export default {
-  more: '更多',
+  ...zh_CN,
+  SearchTable: {
+    more: '更多',
+    settingTips: '列设置',
+    settingModalTitle: '列设置',
+    settingModalOk: '确定',
+    settingModalReset: '重置本次设置',
+    settingModalResetDefault: '恢复默认设置',
+    settingModalColumnEnum: ['列名称', '显示列', '宽度', '固定方式', '排序'],
+    longText: '全部',
+    longTextModalTitle: '全部',
+  },
 }
